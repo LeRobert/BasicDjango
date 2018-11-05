2a5d2db9dbd5b549270aff63bbfb118c7a8668a2
Robert Leskovar <robert.leskovar@gmail.com>
2018-11-05 15:30:28 +0100

    Add project files.

---
+++ BasicDjango/BasicDjango/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for BasicDjango.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', BasicDjango.views.home, name='home'),
+    # url(r'^BasicDjango/', include('BasicDjango.BasicDjango.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
